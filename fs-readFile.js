1bcea6859a833b6a91b501f44bd7e02088174c22
EnriquezAlberto <betooenriquez6234@gmail.com>
2023-07-21 06:06:45 -0300

    fs modos

---
+++ fs-readFile.js
@@ -0,0 +1,15 @@
+const fs = require('node:fs');
+
+console.log('leyendo el primer archivo...');
+
+const text = fs.readFile('./archivo.txt', 'utf8', (err,text) => {
+    console.log(text);
+});
+
+
+const text2 = fs.readFile('./archivo2.txt', 'utf8',(err,text) => {
+    console.log('leyendo el segundo...');
+    console.log(text);
+});
+
+
