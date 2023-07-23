a0211f97585b66ba4c7feaf7532992241fe209e7
EnriquezAlberto <betooenriquez6234@gmail.com>
2023-07-23 15:16:39 -0300

    modificacion

---
+++ process.js
@@ -0,0 +1,18 @@
+//process
+const process = require('node:process')
+console.log(process.argv)
+
+//control de eventos y salidas
+process.exit(0)  //1, 2
+
+//process events
+process.on('exit', () => {
+    console.log('terminado')
+})
+
+
+//current working directory, recuperar proceso dependiendo la carpeta
+
+console.log(process.cwd())
+
+console.log(process.env.hate)
