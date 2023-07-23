ef17831b87b5f4fd5388a0b210fc2aaebb7a7c62
EnriquezAlberto <betooenriquez6234@gmail.com>
2023-07-23 04:05:43 -0300

    fs/promises

---
+++ fs-promises.js
@@ -0,0 +1,17 @@
+const fs = require('node:fs/promises');
+const {promisify} = require('node:util');
+
+const readFile = promisify(fs.readFile);
+
+console.log('leyendo el primer archivo...');
+
+const text = fs.readFile('./archivo.txt', 'utf8').then(text => {
+    console.log('primero:',text);
+});
+
+console.log('leyendo el segundo...');
+
+const text2 = fs.readFile('./archivo2.txt', 'utf8').then(text => {
+    console.log('segundo:',text);
+});
+
